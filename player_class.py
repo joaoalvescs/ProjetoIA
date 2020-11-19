9978274ec651b1fbbe259bf6987344de9cb4d246
Gilson Caio <gilsoncaio7@gmail.com>
2020-11-18 22:06:08 -0300

    Limitando a movimentacao do jogador ao grid (Finalizando - Parte 08)

--- player_class.py
+++ player_class.py
@@ -8,9 +8,11 @@ class Player:
     self.grid_pos = pos
     self.pix_pos = self.get_pix_pos()
     self.direction = vec(1,0)
+    self.stored_direction = None
+
+
     self.angle = -90 #Angulo inicial da sprite do Jogador (virada para a direita)
     self.last_key_pressed = 'right' #Direção inicial do Jogador sendo registrada como uma tecla pressionada
-
     self.PLAYER_SPRITE = pygame.image.load('./sprites/player.png').convert_alpha()
     self.PLAYER_SPRITE = pygame.transform.scale(self.PLAYER_SPRITE, (self.app.cell_width, self.app.cell_height))
     self.PLAYER_SPRITE = pygame.transform.rotate(self.PLAYER_SPRITE, self.angle)
@@ -18,6 +20,18 @@ class Player:
 
   def update(self):
     self.pix_pos += self.direction
+
+    # Mover-se apenas dentro do grid
+    # Eixo X
+    if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+      if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+        if self.stored_direction != None:
+          self.direction = self.stored_direction
+    # Eixo Y
+    if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+      if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+        if self.stored_direction != None:
+          self.direction = self.stored_direction
 
     # Configurando grid_pos em relação a pix_pos
     self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width+1
@@ -36,7 +50,7 @@ class Player:
 
   def move(self, direction, angle):
     #Contem a direcao e o angulo, que vai ser usado para rotacionar a sprite
-    self.direction = direction
+    self.stored_direction = direction
     self.PLAYER_SPRITE = pygame.transform.rotate(self.PLAYER_SPRITE, angle)
 
 
