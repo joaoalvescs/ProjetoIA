b0494f28abab536171218682ef321b5e493e8b01
AlissonRodrigo43 <45495964+AlissonRodrigo43@users.noreply.github.com>
2020-11-19 13:23:02 -0300

    Criando interação do jogador com os muros

--- player_class.py
+++ player_class.py
@@ -9,7 +9,7 @@ class Player:
     self.pix_pos = self.get_pix_pos()
     self.direction = vec(1,0)
     self.stored_direction = None
-
+    self.able_to_move = True
 
     self.angle = -90 #Angulo inicial da sprite do Jogador (virada para a direita)
     self.last_key_pressed = 'right' #Direção inicial do Jogador sendo registrada como uma tecla pressionada
@@ -19,20 +19,13 @@ class Player:
  
 
   def update(self):
-    self.pix_pos += self.direction
-
+    if self.able_to_move:
+      self.pix_pos += self.direction
     # Mover-se apenas dentro do grid
-    # Eixo X
-    if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
-      if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+    if self.time_to_move():
         if self.stored_direction != None:
           self.direction = self.stored_direction
-    # Eixo Y
-    if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
-      if self.direction == vec(0, 1) or self.direction == vec(0, -1):
-        if self.stored_direction != None:
-          self.direction = self.stored_direction
-
+        self.able_to_move = self.can_move()
     # Configurando grid_pos em relação a pix_pos
     self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width+1
     self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER+self.app.cell_height//2)//self.app.cell_height+1
@@ -57,3 +50,19 @@ class Player:
   def get_pix_pos(self):
     return vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+self.app.cell_width//2,
     (self.grid_pos.y*self.app.cell_height)+TOP_BOTTOM_BUFFER//2+self.app.cell_height//2)
+
+  def time_to_move(self):
+      #Eixo X
+      if int(self.pix_pos.x + TOP_BOTTOM_BUFFER // 2) % self.app.cell_width == 0:
+          if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+              return True
+      #Eixo Y
+      if int(self.pix_pos.y + TOP_BOTTOM_BUFFER // 2) % self.app.cell_height == 0:
+          if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+              return True
+
+  def can_move(self):
+      for wall in self.app.walls:
+          if vec(self.grid_pos+self.direction) == wall:
+              return False
+      return True
